8148f300b5b31610e25bbef076e7c4f504d86955
Clayton <cnewmiller@gmail.com>
2021-11-16 11:57:54 -0600

    downgraded records

--- framework/src/main/java/edu/depaul/group14/FinalsWeekTest.java
+++ framework/src/main/java/edu/depaul/group14/FinalsWeekTest.java
@@ -1,6 +1,7 @@
 package edu.depaul.group14;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -10,7 +11,62 @@ import edu.depaul.group14.core.StatProcessor.Statistic;
 public interface FinalsWeekTest<M> {
 
 
-    record Run<M, O>(long start, long end, M message, Optional<O> output) {}
+    final class Run<M, O> {
+        private final long start;
+        private final long end;
+        private final M message;
+        private final Optional<O> output;
+
+        public Run(long start, long end, M message, Optional<O> output) {
+            this.start = start;
+            this.end = end;
+            this.message = message;
+            this.output = output;
+        }
+
+        public long start() {
+            return start;
+        }
+
+        public long end() {
+            return end;
+        }
+
+        public M message() {
+            return message;
+        }
+
+        public Optional<O> output() {
+            return output;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this)
+                return true;
+            if (obj == null || obj.getClass() != this.getClass())
+                return false;
+            var that = (Run) obj;
+            return this.start == that.start &&
+                   this.end == that.end &&
+                   Objects.equals(this.message, that.message) &&
+                   Objects.equals(this.output, that.output);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(start, end, message, output);
+        }
+
+        @Override
+        public String toString() {
+            return "Run[" +
+                   "start=" + start + ", " +
+                   "end=" + end + ", " +
+                   "message=" + message + ", " +
+                   "output=" + output + ']';
+        }
+    }
 
     void init();
 
